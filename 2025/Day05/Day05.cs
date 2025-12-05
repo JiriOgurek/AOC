7e3a2b2704d28d662a3ed6412309b619eacb0a78
Jiří Ogurek <j_ogurek@seznam.cz>
2025-12-05 15:05:00 +0100

    2025\Day05\Part1

---
+++ 2025/Day05/Day05.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace AOC2025.Day05
+{
+    internal static class Day05
+    {
+        public static void Solve()
+        {
+            var input = File.ReadAllLines(@"Day05\input.txt");
+
+
+            var stopwatch = Stopwatch.StartNew();
+            PartOne(input);
+            stopwatch.Stop();
+            Console.WriteLine($"PartOne execution time: {stopwatch.Elapsed.TotalMilliseconds} ms");
+
+            stopwatch = Stopwatch.StartNew();
+            PartTwo(input);
+            stopwatch.Stop();
+            Console.WriteLine($"PartTwo execution time: {stopwatch.Elapsed.TotalMilliseconds} ms");
+        }
+
+        private static void PartTwo(string[] input)
+        {
+            var secondHalf = false;
+
+            //read all ranges and ids
+            var ranges = new List<LongRange>();
+            var ids = new List<long>();
+
+            foreach (var line in input)
+            {
+                if (!secondHalf)
+                {
+                    // first half of the input - ranges
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        secondHalf = true;
+                        continue;
+                    }
+
+                    var parts = line.Split("-", StringSplitOptions.TrimEntries);
+                    if (parts.Length == 2 &&
+                        long.TryParse(parts[0], out var from) &&
+                        long.TryParse(parts[1], out var to))
+                    {
+                        ranges.Add(new LongRange(from, to));
+                    }
+                }
+                else
+                {
+                    // second half of the input - ids
+                    if (long.TryParse(line, out var id))
+                    {
+                        ids.Add(id);
+                    }
+                }
+            }
+
+            var l = new List<long>();
+            foreach (var r in ranges) {
+                for (var i = r.From; i <= r.To; i++) {
+                    if (!l.Contains(i))
+                        l.Add(i);
+                }
+            }
+
+            Console.WriteLine($"Part Two: {l.Count}");
+        }
+
+        private static void PartOne(string[] input)
+        {
+            var secondHalf = false;
+
+            //read all ranges and ids
+            var ranges = new List<LongRange>();
+            var ids = new List<long>();
+
+            foreach (var line in input)
+            {
+                if (!secondHalf)
+                {
+                    // first half of the input - ranges
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        secondHalf = true;
+                        continue;
+                    }
+
+                    var parts = line.Split("-", StringSplitOptions.TrimEntries);
+                    if (parts.Length == 2 &&
+                        long.TryParse(parts[0], out var from) &&
+                        long.TryParse(parts[1], out var to))
+                    {
+                        ranges.Add(new LongRange(from, to));
+                    }
+                }
+                else
+                {
+                    // second half of the input - ids
+                    if (long.TryParse(line, out var id))
+                    {
+                        ids.Add(id);
+                    }
+                }
+            }
+
+            var fresh = 0;
+
+            foreach (var id in ids)
+            {
+                var isValid = false;
+                foreach (var range in ranges)
+                {
+                    if (range.Contains(id))
+                    {
+                        isValid = true;
+                        break;
+                    }
+                }
+                
+                if (isValid)
+                {
+                    fresh++;
+                }
+            }
+
+            Console.WriteLine($"Part One: {fresh}");
+        }
+
+        public readonly struct LongRange
+        {
+            public long From { get; }
+            public long To { get; }
+
+            public LongRange(long from, long to)
+            {
+                if (from > to)
+                    throw new ArgumentException("From must be <= To.");
+
+                From = from;
+                To = to;
+            }
+
+            public bool Contains(long value)
+                => value >= From && value <= To;
+
+            public override string ToString() => $"{From}..{To}";
+        }
+    }
+}
--- 2025/Program.cs
+++ 2025/Program.cs
@@ -2,8 +2,10 @@
 using AOC2025.Day02;
 using AOC2025.Day03;
 using AOC2025.Day04;
+using AOC2025.Day05;
 
 //Day01.Solve();
 //Day02.Solve();
 //Day03.Solve();
-Day04.Solve();
+//Day04.Solve();
+Day05.Solve();
