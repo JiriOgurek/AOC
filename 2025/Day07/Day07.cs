69633f808157c65cb9231af4d46ca623d4db9010
Jiří Ogurek <j_ogurek@seznam.cz>
2025-12-13 20:44:06 +0100

    2025\Day07 Init

---
+++ 2025/Day07/Day07.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AOC2025.Day07
+{
+    internal static class Day07
+    {
+        public static void Solve()
+        {
+            var input = File.ReadAllLines(@"Day07\input.txt");
+
+            var stopwatch = Stopwatch.StartNew();
+            PartOne(input);
+            stopwatch.Stop();
+            Console.WriteLine($"PartOne execution time: {stopwatch.Elapsed.TotalMilliseconds} ms");
+
+            //stopwatch = Stopwatch.StartNew();
+            //PartTwo(input);
+            //stopwatch.Stop();
+            //Console.WriteLine($"PartTwo execution time: {stopwatch.Elapsed.TotalMilliseconds} ms");
+        }
+
+        private static void PartOne(string[] input)
+        {
+
+        }
+    }
+}
--- 2025/Program.cs
+++ 2025/Program.cs
@@ -4,10 +4,12 @@ using AOC2025.Day03;
 using AOC2025.Day04;
 using AOC2025.Day05;
 using AOC2025.Day06;
+using AOC2025.Day07;
 
 //Day01.Solve();
 //Day02.Solve();
 //Day03.Solve();
 //Day04.Solve();
 //Day05.Solve();
-Day06.Solve();
+//Day06.Solve();
+Day07.Solve();
